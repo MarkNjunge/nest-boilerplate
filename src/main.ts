02613f4a8d98198f299f9ea0f8252baf8fa13eb9
MarkNjunge <mark.kamau@outlook.com>
2019-07-11 15:44:11 +0300

    Add rate limiter

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,26 @@ import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
 import { ValidationPipe } from "./common/pipes/validation.pipe";
 import { config } from "./common/Config";
-import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import * as fastifyRateLimit from "fastify-rate-limit";
 
 async function bootstrap() {
   initializeWinston();
 
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
-    logger: new CustomLogger("NestApplication"),
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      logger: new CustomLogger("NestApplication"),
+    },
+  );
+
+  app.register(fastifyRateLimit, {
+    max: 100,
+    timeWindow: 1000 * 60,
   });
 
   app.enableCors({
