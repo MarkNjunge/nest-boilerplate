447625faabfba75bbf750480ed405ec5e85c56dc
MarkNjunge <mark.kamau@outlook.com>
2019-08-08 11:29:50 +0300

    Update CORS config

--- src/main.ts
+++ src/main.ts
@@ -39,8 +39,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Accept",
+    methods: "*",
+    allowedHeaders: "*",
   });
 
   app.useGlobalFilters(new HttpExceptionFilter());
