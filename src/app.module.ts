0dddf64d570a6d8630d67ac8649df0b29acf3401
MarkNjunge <mark.kamau@outlook.com>
2020-04-04 18:15:12 +0300

    Use glob file pattern for entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { config } from "./common/Config";
 import { UsersModule } from "./users/users.module";
 import * as path from "path";
-import { UserEntity } from "./users/entitiy/User.entity";
-import { AddressEntity } from "./users/entitiy/Address.entity";
-import { ContactEntity } from "./users/entitiy/Contact.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "postgres",
       url: config.db.url,
-      entities: [UserEntity, AddressEntity, ContactEntity],
+      entities: [path.join(__dirname, "./**/*.entity{.ts,.js}")],
       migrations: [path.join(__dirname, "./db/migration/*{.ts,.js}")],
       migrationsRun: true,
       synchronize: false,
