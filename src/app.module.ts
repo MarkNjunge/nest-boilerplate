cac35c94d1999d868db2fb2d7e2bd66a17675c6c
MarkNjunge <mark.kamau@outlook.com>
2021-07-28 16:02:17 +0300

    Enable db ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,17 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { config } from "./config";
 import { UsersModule } from "./modules/users/users.module";
 import * as path from "path";
+import { TlsOptions } from "tls";
+
+let ssl: boolean | TlsOptions = config.db.ssl;
+if (ssl === true) {
+  ssl = {
+    // This accepts a self signed certificate
+    // See node-postgres docs for how to verify
+    // https://node-postgres.com/features/ssl
+    rejectUnauthorized: false,
+  };
+}
 
 @Module({
   imports: [
@@ -15,15 +26,7 @@ import * as path from "path";
       migrations: [path.join(__dirname, "./db/migration/*{.ts,.js}")],
       migrationsRun: true,
       synchronize: false,
-      extra: {
-        ssl: {
-          require: config.db.ssl,
-          // This accepts a self signed certficate
-          // See node-postgres docs for how to verify
-          // https://node-postgres.com/features/ssl
-          rejectUnauthorized: false,
-        },
-      },
+      ssl,
     }),
     UsersModule,
   ],
