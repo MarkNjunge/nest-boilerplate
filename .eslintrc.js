72542abe989cad8c46ad961a27b05ab37bf161c0
MarkNjunge <mark.kamau@outlook.com>
2022-11-01 21:06:44 +0300

    Update Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,20 +16,8 @@ module.exports = {
     jest: true,
   },
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/no-unsafe-argument": "off",
-    "@typescript-eslint/restrict-template-expressions": ["error", { allowAny: true }],
-    "eqeqeq": "off",
-    "@typescript-eslint/no-unsafe-return": "off",
     "padding-line-between-statements": "off",
     "@typescript-eslint/indent": "off",
     "@typescript-eslint/no-unnecessary-condition": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/lines-between-class-members": "off"
   },
 };
