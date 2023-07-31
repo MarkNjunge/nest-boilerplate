4bef4f0732c37ef6025ea0b5fbfcec1a048b7671
MarkNjunge <mark.kamau@outlook.com>
2023-07-31 17:34:54 +0300

    Improve extracting IP

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -1,5 +1,14 @@
 import { FastifyRequest } from "fastify";
 
 export function extractIp(request: FastifyRequest): string {
-  return (request.headers["x-real-ip"] ?? request.headers["x-forwarded-for"] ?? request.ip) as string;
+  const headers = request.headers;
+  let ip = request.ip;
+  if (headers["cf-connecting-ip"]) {
+    ip = headers["cf-connecting-ip"] as string;
+  } else if (headers["x-real-ip"]) {
+    ip = headers["x-real-ip"] as string;
+  } else if (headers["x-forwarded-for"]) {
+    ip = (headers["x-forwarded-for"] as string).split(",")[0];
+  }
+  return ip;
 }
