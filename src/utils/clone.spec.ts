1af6a7b7c9ab2f1b3ea7a56f95bd9e8190d316c7
MarkNjunge <mark.kamau@outlook.com>
2023-11-15 16:09:51 +0300

    Update clone to handle cyclic dependencies

---
+++ src/utils/clone.spec.ts
@@ -0,0 +1,67 @@
+import { clone, isCyclic } from "@/utils/clone";
+
+describe("clone", () => {
+  describe("isCyclic", () => {
+    it("returns true for cyclic", () => {
+      const x: any = {
+        a: "b",
+        c: {
+          a: "b",
+        },
+      };
+      x.x = x;
+
+      expect(isCyclic(x)).toBe(true);
+    });
+
+    it("returns true for cyclic arrays", () => {
+      const x: any = [
+        {
+          a: "b",
+          c: {
+            a: "b",
+          },
+        },
+      ];
+      x[0].x = x[0];
+
+      expect(isCyclic(x)).toBe(true);
+    });
+
+    it("returns false for non-cyclic", () => {
+      const x: any = {
+        a: "b",
+        c: {
+          a: "b",
+        },
+      };
+
+      expect(isCyclic(x)).toBe(false);
+    });
+  });
+
+  describe("clone", () => {
+    it("can clone", () => {
+      const x: any = {
+        a: "b",
+        c: {
+          a: "b",
+        },
+      };
+
+      expect(clone(x)).not.toBe(x);
+    });
+
+    it("can clone circular references", () => {
+      const x: any = {
+        a: "b",
+        c: {
+          a: "b",
+        },
+      };
+      x.x = x;
+
+      expect(clone(x)).not.toBe(x);
+    });
+  });
+});
--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -1,12 +1,69 @@
-export function clone(obj: any): any {
+export function clone<T = any>(obj: T, checkCyclic = true): T {
+  // It's faster to check if the object is cyclic and switch, than to always use JSON
+  if (checkCyclic && isCyclic(obj)) {
+    return cloneJson<T>(obj);
+  }
+
   if (obj == null || typeof obj != "object") {
     return obj;
   }
 
-  const temp = new obj.constructor();
+  const temp = new (obj as any).constructor();
   for (const key in obj) {
     temp[key] = clone(obj[key]);
   }
 
   return temp;
+}
+
+function cloneJson<T = any>(obj: T): T {
+  const cache: any[] = [];
+  const str = JSON.stringify(obj, (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        // Circular reference found, discard key
+        return;
+      }
+      // Store value in our collection
+      cache.push(value);
+    }
+    return value;
+  });
+
+  return JSON.parse(str);
+}
+
+/**
+ * Check if an object has cyclic dependencies
+ * @see https://stackoverflow.com/a/41292020
+ * @param obj
+ */
+export function isCyclic(obj: any): boolean {
+  const stackSet = new Set();
+  let detected = false;
+
+  function detect(obj: any) {
+    if (!(obj instanceof Object)) {
+      return;
+    }
+
+    if (stackSet.has(obj)) {
+      detected = true;
+      return;
+    }
+
+    stackSet.add(obj);
+    for (const k in obj) {
+      if (obj.hasOwnProperty(k)) {
+        detect(obj[k]);
+      }
+    }
+
+    stackSet.delete(obj);
+    return;
+  }
+
+  detect(obj);
+
+  return detected;
 }
