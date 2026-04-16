5c5712b9b32430d78e6564f2c6cc697e334bd26a
MarkNjunge <git@marknjunge.com>
2026-04-16 17:00:22 +0300

    Revert "Use monotonic ULIDs for db ids"
    This reverts commit 9d031835483d00e5820e2040a51bef866e5e649c.

--- src/lib/crud/entity/id.ts
+++ src/lib/crud/entity/id.ts
@@ -1,7 +1,6 @@
-import { monotonicFactory } from "ulid";
+import { ulid } from "ulid";
 
 const seedTime = new Date("2025-01-01T00:00:00.000Z").getTime();
-const ulid = monotonicFactory();
 
 export function genId(prefix: string): string {
   return `${prefix}${ulid(seedTime).toLowerCase()}`;
