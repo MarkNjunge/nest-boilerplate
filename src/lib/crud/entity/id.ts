9d031835483d00e5820e2040a51bef866e5e649c
MarkNjunge <git@marknjunge.com>
2026-04-16 16:02:05 +0300

    Use monotonic ULIDs for db ids

--- src/lib/crud/entity/id.ts
+++ src/lib/crud/entity/id.ts
@@ -1,6 +1,7 @@
-import { ulid } from "ulid";
+import { monotonicFactory } from "ulid";
 
 const seedTime = new Date("2025-01-01T00:00:00.000Z").getTime();
+const ulid = monotonicFactory();
 
 export function genId(prefix: string): string {
   return `${prefix}${ulid(seedTime).toLowerCase()}`;
