28c693668e3819516fc8d80bbcd5b558e7561464
MarkNjunge <git@marknjunge.com>
2026-05-26 16:35:48 +0300

    Add swagger decorators for base entity fields

--- src/lib/crud/entity/base.entity.ts
+++ src/lib/crud/entity/base.entity.ts
@@ -1,15 +1,19 @@
 import { BeforeInsert, Column, PrimaryColumn } from "typeorm";
 import { genId } from "@/lib/crud/entity/id";
+import { ApiProperty } from "@nestjs/swagger";
 
 export abstract class BaseEntity {
   abstract idPrefix(): string;
 
+  @ApiProperty()
   @PrimaryColumn({ type: "varchar" })
   id: string;
 
+  @ApiProperty()
   @Column({ type: "timestamptz", name: "created_at", default: () => "now()", update: false })
   createdAt: Date;
 
+  @ApiProperty()
   @Column({ type: "timestamptz", name: "updated_at", default: () => "now()" })
   updatedAt: Date;
 
